002702f94fff4597f0e7e76bdcbc50b2720bd556
KC-Nick <wilson.kasinichole@gmail.com>
2024-01-01 23:18:11 -0500

    added controller and route files to add code to, finished connection code so server connects to MongoDB

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,15 @@
 const { connect, connection } = require('mongoose');
 
-const connectionString = 'mongodb://localhost:27017/socialnetDB';
+const connectionString = 'mongodb://127.0.0.1:27017/';
 
 connect(connectionString);
+
+connection.on('connected', () => {
+  console.log('Connected to MongoDB');
+});
+
+connection.on('error', (error) => {
+  console.error('MongoDB connection error:', error);
+});
 
 module.exports = connection;
